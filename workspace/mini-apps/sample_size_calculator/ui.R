7d27c035dace7d73bf57ca8a53db15e4cb508161
James Bonner <james.bonner@aridhia.com>
2017-06-14 11:00:26 +0100

    update ui and stylesheet

--- workspace/mini-apps/sample_size_calculator/ui.R
+++ workspace/mini-apps/sample_size_calculator/ui.R
@@ -17,8 +17,8 @@ shinyUI(fluidPage( theme = "xapstyles.css",
                       fluidPage(
                       h3("Study Type"),
                       p(""),
-             checkboxInput(inputId = "clustered", label = "Clustered Design?", value = FALSE),
-             checkboxInput(inputId = "binary", label = "Binary Dependent Variable?", value = FALSE),
+             checkboxInput(inputId = "clustered", label = "Clustered design?", value = FALSE),
+             checkboxInput(inputId = "binary", label = "Binary dependent variable?", value = FALSE),
              br(),
            
     
@@ -43,13 +43,13 @@ shinyUI(fluidPage( theme = "xapstyles.css",
            fluidPage(width = 12,
             fluidRow(column(
               6,
-              h3("Calculating Required Study Sample Size"),
+              h3("Calculating required study sample size"),
               p("This mini-app acts as a calculator, helping you understand the power of your
                 experimental design to detect treatment effects. This will inform the sample 
                 size you require to ensure your study is sufficiently powered."),
               br(),
-              h4("App layout"),
-              p("The app contains two tabs; this Help tab gives you an overview 
+              h4("Mini-app layout"),
+              p("The mini-app contains two tabs; this Help tab gives you an overview 
                 of the app itself. The Application tab allows you to select and run the 
                 required study type."),
               br(),
@@ -57,7 +57,7 @@ shinyUI(fluidPage( theme = "xapstyles.css",
               p("To experiment with how study type affects the required sample size, you can: "),
               tags$ol(
                 tags$li("Choose a ", strong("standard design "), 
-                        "design in which individuals are randomly assigned to treatment or control."),
+                        "in which individuals are randomly assigned to treatment or control."),
                 tags$li("Choose a ", strong("clustered design"), "in which groups of individuals are assigned to treatment and control together."),
                 tags$li("Opt for a ", strong("binary study design"), "with a binary outcome assessed between a control and treated group.")
               ),
@@ -68,10 +68,10 @@ shinyUI(fluidPage( theme = "xapstyles.css",
             column(
               6,
               h3("Walkthrough Video"), 
-              HTML('<iframe width="100%" height="300" src="//www.youtube.com/embed/RWCoExyMIrI?rel=0" frameborder="0" allowFullScreen=""></iframe>'),
+              HTML('<iframe width="100%" height="300" src="//www.youtube.com/embed/SR0bJS65heU?rel=0" frameborder="0" allowFullScreen=""></iframe>'),
               br(),
-              p(strong("NB: This mini-app is for provided for demonstration purposes, is unsupported and is utilised at user's risk. If you plan to
-                       use this mini-app to inform your study, please review the code and ensure you are comfortable with the calculations made before proceeding. "))
+              p("NB: This mini-app is for provided for demonstration purposes, is unsupported and is utilised at user's risk. If you plan to
+                       use this mini-app to inform your study, please review the code and ensure you are comfortable with the calculations made before proceeding. ")
             ))
           
            
